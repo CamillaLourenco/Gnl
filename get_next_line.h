7bf53945865e5c965a89ca33b75fbe47c2b5a247
Caos <camillalourenco@outlook.com>
2023-01-18 17:55:19 +0000

    project

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clourenc  <clourenc@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 19:36:28 by clourenc          #+#    #+#             */
+/*   Updated: 2022/08/11 22:34:37 by clourenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *c);
+char	*ft_strchr(const char *s, int c);
+
+#endif
